427c49a5435ae3fa5c621a6e2412a3e9a5b4855a
tjdanielson <tessajdanielson@gmail.com>
2022-03-02 13:25:59 -0600

    Setup Customer class

--- customer.py
+++ customer.py
@@ -1,3 +1,18 @@
+from shopping_cart import ShoppingCart
 
 class Customer:
-    pass
+    
+    def __init__(self, name):
+        self.name = name
+        self.shopping_cart = ShoppingCart
+
+    def add_product_to_cart(self):
+        ShoppingCart.add_new_product
+
+    def view_cart(self):
+        for product in ShoppingCart.products:
+            print(product)
+
+    
+
+
